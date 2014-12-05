663aa5c621a5e7a91a429ba1f848f2a0a5f7e826
Martin Morgan <mtmorgan@fhcrc.org>
2014-12-04 18:31:06 -0800

    list entire locations (missing 'container')

--- R/swlist.R
+++ R/swlist.R
@@ -1,22 +1,34 @@
+.swnull <- function(x, as=NA_character_) {
+    if (is.list(x)) {
+        mapply(function(x) if (is.null(x)) as else x, x)
+    } else {
+        x[is.null(x)] <- as
+        x
+    }
+}
+
 swlist <-
     function(container, format=c("short", "long"))
 {
-    stopifnot(.isSingleString(container))
+    stopifnot(missing(container) || .isSingleString(container))
     format <- match.arg(format)
 
     curl <- RCurl::getCurlHandle()
     hdr <- .swauth(curl)
 
-    url <- sprintf("%s/%s?format=json", hdr[["X-Storage-Url"]], container)
+    url <- if (missing(container)) {
+        sprintf("%s?format=json", hdr[["X-Storage-Url"]])
+    } else {
+        sprintf("%s/%s?format=json", hdr[["X-Storage-Url"]], container)
+    }
     auth <- sprintf("%s: %s", "X-Auth-Token", hdr[["X-Storage-Token"]])
     objects <- GET(url, config(httpheader=auth), curl=curl)
     stop_for_status(objects)
 
     content <- content(objects)
-
     bytes <- sapply(content, "[[", "bytes")
-    last_modified <- sapply(content, "[[", "last_modified")
-    name <- sapply(content, "[[", "name")
+    last_modified <- .swnull(sapply(content, "[[", "last_modified"))
+    name <- .swnull(sapply(content, "[[", "name"))
 
     switch(format, short={
         FUN <- utils:::format.object_size
@@ -24,7 +36,7 @@ swlist <-
         data.frame(size=size, last_modified=last_modified, name=name,
                    stringsAsFactors=FALSE)
     }, long={
-        hash <- sapply(content, "[[", "hash")
+        hash <- .swnull(sapply(content, "[[", "hash"))
         data.frame(bytes=bytes, last_modified=last_modified, hash=hash,
                    name=name, stringsAsFactors=FALSE)
     })
